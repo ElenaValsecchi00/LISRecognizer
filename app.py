bbb261431ea70d94af1f4b2adbf895de06ad44b1
ElenaValsecchi00 <elena.valsecchi12@gmail.com>
2024-02-21 14:52:39 +0100

    two modalities

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import csv
 import copy
 import argparse
 import imutils
-import random
+from random import choice
 import os
 from collections import Counter
 from collections import deque
@@ -18,49 +18,23 @@ from lis_classifier_moving import LISClassifierMoving
 
 from data_parsing import create_distance_vector
 
-
-def get_args():
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument("--device", type=int, default=0)
-    parser.add_argument("--width", help='cap width', type=int, default=960)
-    parser.add_argument("--height", help='cap height', type=int, default=540)
-
-    parser.add_argument('--use_static_image_mode', action='store_true')
-    parser.add_argument("--min_detection_confidence",
-                        help='min_detection_confidence',
-                        type=float,
-                        default=0.7)
-    parser.add_argument("--min_tracking_confidence",
-                        help='min_tracking_confidence',
-                        type=int,
-                        default=0.5)
-
-    args = parser.parse_args()
-
-    return args
-
-
 def main():
-    # Argument parsing #################################################################
-    args = get_args()
+    cap_device = 0
+    cap_width = 960
+    cap_height = 540
 
-    cap_device = args.device
-    cap_width = args.width
-    cap_height = args.height
-
-    use_static_image_mode = args.use_static_image_mode
-    min_detection_confidence = args.min_detection_confidence
-    min_tracking_confidence = args.min_tracking_confidence
+    use_static_image_mode = False
+    min_detection_confidence = 0.7
+    min_tracking_confidence = 0.5
 
     use_brect = True
 
-    # Camera preparation ###############################################################
+    # Camera preparation
     cap = cv.VideoCapture(cap_device)
     cap.set(cv.CAP_PROP_FRAME_WIDTH, cap_width)
     cap.set(cv.CAP_PROP_FRAME_HEIGHT, cap_height)
 
-    # Model load #############################################################
+    # Model load 
     mp_hands = mp.solutions.hands
     hands = mp_hands.Hands(
         static_image_mode=use_static_image_mode,
@@ -72,7 +46,7 @@ def main():
     lis_classifier = LISClassifier()
     lis_classifier_moving = LISClassifierMoving()
 
-    # Read labels ###########################################################
+    # Read labels 
     with open(
             'lis_classifier_label.csv',
             encoding='utf-8-sig') as f:
@@ -88,10 +62,11 @@ def main():
             row[0] for row in lis_classifier_moving_labels
         ]
 
-    # FPS Measurement ########################################################
-    cvFpsCalc = CvFpsCalc(buffer_len=10)
+    #Test words
+    test_words = ["spell your name", "spell grazie", "spell casa", "spell rana", "spell corsa"]
+    word_to_spell = 0
 
-    # Coordinate history #################################################################
+    # Coordinate history ######
     history_length = 6
     point_history = deque(maxlen=history_length)
 
@@ -101,16 +76,21 @@ def main():
     
     word = ["None"]
 
-    #  ########################################################################
+    mode = 0
+    #  #############
 
     while True:
 
-        # Process Key (ESC: end) #################################################
+        # Process Key
         key = cv.waitKey(10)
         if key == 27:  # ESC
             break
+        if key == 49:
+            mode = 1
+        if key == 48:
+            mode = 0 
         
-        # Camera capture #####################################################
+        # Camera capture 
         ret, image = cap.read()
         image = imutils.resize(image, width=1000)
         if not ret:
@@ -118,14 +98,14 @@ def main():
         image = cv.flip(image, 1)  # Mirror display
         debug_image = copy.deepcopy(image)
 
-        # Detection implementation #############################################################
+        # Detection implementation ##
         image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
 
         image.flags.writeable = False
         results = hands.process(image)
         image.flags.writeable = True
 
-        #  ####################################################################
+        #  #########
         if results.multi_hand_landmarks is not None:
             for hand_landmarks, handedness in zip(results.multi_hand_landmarks,
                                                   results.multi_handedness):
@@ -152,8 +132,14 @@ def main():
                     finger_gesture_id, percentage_moving = lis_classifier_moving(
                         point_history_list)
 
-    
-                sign = get_template(hand_sign_id) #get the file with the guessed sign
+                #Get the file with the guessed sign
+                sign = get_template(hand_sign_id, 'sign_photos') 
+                if finger_gesture_id == 0:
+                    moving_sign = get_template(0, 'moving_sign_photos')
+                elif finger_gesture_id == 3:
+                    moving_sign = get_template(1, 'moving_sign_photos')
+                else:
+                    moving_sign = ""
 
                 # Drawing part
                 debug_image = draw_bounding_rect(use_brect, debug_image, brect)
@@ -166,21 +152,29 @@ def main():
                     lis_classifier_moving_labels[finger_gesture_id], 
                     percentage,
                     percentage_moving,
-                    sign
+                    sign, 
+                    moving_sign,
+                    mode,
+                    word_to_spell,
+                    test_words
                 )
                 
-                #Construction of the word
-                hand_gesture_history.append(letter)
-                h_g = list(hand_gesture_history)
-                if len(h_g) > 0 and h_g.count(h_g[0]) == hand_history_length:
-                    if letter == "si":
-                        word = ["None"]
-                    elif word[-1]!= letter:
-                        word.append(letter)
+                if mode == 1:
+                    #Construction of the word
+                    hand_gesture_history.append(letter)
+                    h_g = list(hand_gesture_history)
+                    if len(h_g) > 0 and h_g.count(h_g[0]) == hand_history_length:
+                        if letter == "si":
+                            word = ["None"]
+                            if mode == 1:
+                                word_to_spell = choice([i for i in range(0,len(test_words)) if i != word_to_spell])
 
-                debug_image = draw_word(word,debug_image)
+                        elif word[-1]!= letter:
+                            word.append(letter)
+
+                    debug_image = draw_word(word,debug_image)
         
-        # Screen reflection #############################################################
+        # Screen reflection 
         cv.imshow('Hand Gesture Recognition', debug_image)
 
     cap.release()
@@ -230,10 +224,10 @@ def draw_bounding_rect(use_brect, image, brect):
     return image
 
 #Function that returns right image for the sign
-def get_template(indx):
-    signs = os.listdir('sign_photos')
+def get_template(indx, path):
+    signs = os.listdir(path)
     sign = signs[indx]
-    return "sign_photos/"+sign
+    return path+"/"+sign
 
 #writes the word the user is spelling
 def draw_word(word,image):
@@ -249,15 +243,17 @@ def draw_word(word,image):
     return image
 
 
- #Display all the info on capture
-def draw_info_text(image, brect, handedness, hand_sign_text, moving_text, percentage, percentage_moving,sign):
+#Display all the info on capture
+def draw_info_text(image, brect, handedness, hand_sign_text, moving_text,
+                    percentage, percentage_moving,sign, moving_sign,
+                    mode, word_to_spell, test_words):
     cv.rectangle(image, (brect[0], brect[1]), (brect[2], brect[1] - 22),
                  (0, 0, 0), -1)
     info_text = handedness.classification[0].label[0:]
 
     #Determine if similarity threshold is enough
-    h_text = "None" if percentage < 0.75 else hand_sign_text
-    m_text = "None" if percentage_moving < 0.95 else moving_text
+    h_text = "None" if percentage < 0.85 else hand_sign_text
+    m_text = "None" if percentage_moving < 0.99 else moving_text
 
     #If not, remind how letter should be displayed
     try_text = ""
@@ -279,24 +275,38 @@ def draw_info_text(image, brect, handedness, hand_sign_text, moving_text, percen
             info_text = info_text + ':' + h_text
             letter = h_text
     
-
     cv.putText(image, info_text, (brect[0] + 5, brect[1] - 4),
-               cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1, cv.LINE_AA)
-    cv.putText(image,try_text, (10, 70), cv.FONT_HERSHEY_SIMPLEX,
-               1.0, (0,0,0), 4, cv.LINE_AA)
-    cv.putText(image,try_text, (10, 70), cv.FONT_HERSHEY_SIMPLEX,
-               1.0, (255, 255, 255), 2, cv.LINE_AA)
-    cv.putText(image,try_m_text, (10, 110), cv.FONT_HERSHEY_SIMPLEX,
-               1.0, (0,0,0), 4, cv.LINE_AA)
-    cv.putText(image,try_m_text, (10, 110), cv.FONT_HERSHEY_SIMPLEX,
-               1.0, (255, 255, 255), 2, cv.LINE_AA)
+                cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1, cv.LINE_AA)
     
-    
-    #Display image of guessed sign
-    sign = cv.imread(sign)
+    #If mode is free speech 
+    if mode == 0:
+        cv.putText(image,try_text, (10, 70), cv.FONT_HERSHEY_SIMPLEX,
+                1.0, (0,0,0), 4, cv.LINE_AA)
+        cv.putText(image,try_text, (10, 70), cv.FONT_HERSHEY_SIMPLEX,
+                1.0, (255, 255, 255), 2, cv.LINE_AA)
+        cv.putText(image,try_m_text, (470, 70), cv.FONT_HERSHEY_SIMPLEX,
+                1.0, (0,0,0), 4, cv.LINE_AA)
+        cv.putText(image,try_m_text, (470, 70), cv.FONT_HERSHEY_SIMPLEX,
+                1.0, (255, 255, 255), 2, cv.LINE_AA)
+        
+        
+        #Display image of guessed sign
+        sign = cv.imread(sign)
+        if moving_sign!="":
+            moving_sign = cv.imread(moving_sign)
 
-    if h_text=="None":
-        image[0:0+sign.shape[0], 300:300+sign.shape[1]] = sign
+        if h_text=="None":
+            image[0:0+sign.shape[0], 300:300+sign.shape[1]] = sign
+        if m_text=="None" and moving_sign!="" and try_m_text!="":
+            image[0:0+moving_sign.shape[0], 740:740+moving_sign.shape[1]] = moving_sign
+
+    
+    #If mode is spell practice
+    else:
+        cv.putText(image,test_words[word_to_spell], (10, 70), cv.FONT_HERSHEY_SIMPLEX,
+                1.0, (0,0,0), 4, cv.LINE_AA)
+        cv.putText(image,test_words[word_to_spell], (10, 70), cv.FONT_HERSHEY_SIMPLEX,
+                1.0, (255, 255, 255), 2, cv.LINE_AA)
     return image,letter
 
 if __name__ == '__main__':
